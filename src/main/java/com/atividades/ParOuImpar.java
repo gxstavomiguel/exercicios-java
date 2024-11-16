c745aa926af853c9286fe390eef6ec48e0eae1eb
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-16 19:30:21 -0300

    Par ou Impar versão funcional

---
+++ src/main/java/com/atividades/ParOuImpar.java
@@ -0,0 +1,23 @@
+package com.atividades;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ParOuImpar poi = new ParOuImpar();
+
+        System.out.println("Informe um número inteiro");
+        int numero = scanner.nextInt();
+        System.out.println(poi.ParOuImpar(numero));
+    }
+
+    public int ParOuImpar(int numero){
+        if(numero % 2 == 0){
+             System.out.println("Par");
+        }  else {
+            System.out.println("Impar");
+        } return numero;
+    }
+
+}
