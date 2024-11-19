d62125493f1619685598c4ad05e4487815445faa
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-19 02:20:05 -0300

    Ordenando números bubblesort

---
+++ src/main/java/com/atividades/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.atividades;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] conjuntoDeInteiros;
+        conjuntoDeInteiros = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Informe os números inteiros para ordenar: ");
+            conjuntoDeInteiros[i] = scanner.nextInt();
+        }
+        System.out.println("Números ordenados: ");
+        Arrays.stream(conjuntoDeInteiros).sorted().forEach(System.out::println);
+    }
+}
