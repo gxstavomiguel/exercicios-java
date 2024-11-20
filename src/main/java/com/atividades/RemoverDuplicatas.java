27993d984dd3368b9ff3f01e081d8628296ba084
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-20 01:28:58 -0300

    Esse foi realmente díficil
    Took 1 hour 32 minutes

--- src/main/java/com/atividades/RemoverDuplicatas.java
+++ src/main/java/com/atividades/RemoverDuplicatas.java
@@ -1,55 +1,29 @@
 package com.atividades;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class RemoverDuplicatas {
+        public static void main(String[] args) {
 
-    public static void main(String[] args) {
+            ArrayList<String> ArrayListpalavras = new ArrayList<>();
+            ArrayList<String> NovoArrayListpalavras = new ArrayList<>();
 
-        Scanner scanner = new Scanner(System.in);
-        RemoverDuplicatas removerDuplicatadas = new RemoverDuplicatas();
-        String[] palavras = new String[5];
+            Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < palavras.length; i++) {
-            System.out.println("Informe as palavras");
-            palavras[i] = scanner.nextLine().toLowerCase();
-            ;
-        }
-    }
-
-    public void Duplicatas(String[] palavras) {
-        for (int i = 1; i < palavras.length; i++) {
-            //if(palavras[i].equals(palavras[i])){
-            if (palavras[i] == palavras[i]) {
-                String[] novasPalavras = palavras[i].split(" ");
-                System.out.println(Arrays.toString(novasPalavras));
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Informe as palavras: ");
+                ArrayListpalavras.add(scanner.nextLine());
             }
+
+            for (String c : ArrayListpalavras) {
+                if(!NovoArrayListpalavras.contains(c)) {
+                    NovoArrayListpalavras.add(c);
+                };
+            }
+
+            System.out.println("Palavras dentro do ArrayList: " + NovoArrayListpalavras);
         }
     }
-
-}
-
-
-
-
-
-
-//
-//        ArrayList<String> palavras = new ArrayList<>();
-//        Scanner scanner = new Scanner(System.in);
-//        RemoverDuplicatas removerDuplicatadas = new RemoverDuplicatas();
-//
-//        for (int i = 0; i < 5; i++){
-//            System.out.println("Informe as palavras " + palavras.add(scanner.nextLine()));
-//        }
-//        for(int i = 1; i < 5; i++){
-//            if(palavras.get(i).equals(palavras.get(i))){
-//                palavras.remove(i);
-//                System.out.println(Arrays.toString(palavras.toArray()));
-//            }
-//        }
-
 
 
 
