f228fab2e15c4073dc9bbbacbdd3bc537fad8f9f
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-20 01:56:13 -0300

    Classe carro
    Took 7 minutes

---
+++ src/main/java/com/atividades/Carro.java
@@ -0,0 +1,38 @@
+package com.atividades;
+
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private String ano;
+
+    public Carro(String marca, String modelo, String ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+}
