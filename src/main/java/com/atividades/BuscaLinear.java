c15ca457b68e97b94e2cc820ab364daed11b2b7d
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-20 01:49:40 -0300

    Busca Linear refatorado
    Took 6 minutes

--- src/main/java/com/atividades/BuscaLinear.java
+++ src/main/java/com/atividades/BuscaLinear.java
@@ -1,24 +1,27 @@
 package com.atividades;
 
-import java.util.Scanner;
-
 public class BuscaLinear {
     public static void main(String[] args) {
 
+        boolean encontrado = false;
+        int x = 17;
         int[] numeros = {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
         };
-        int x = 17;
 
-        for(int i = 0; i < numeros.length; i++){
-            if(x == numeros[i]){
+        for (int i = 0; i < numeros.length; i++) {
+            if (x == numeros[i]) {
                 System.out.println("O número se encontra na posição: " + i);
-            } else {
-                System.out.println("Elemento não encontrado na lista");
+                encontrado = true;
+                break;
             }
         }
+        if (!encontrado) {
+            System.out.println("Elemento não encontrado na lista");
+        }
+
     }
 
 }
