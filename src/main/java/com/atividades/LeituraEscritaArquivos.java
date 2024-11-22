4171bb7e04d4d7e15af79cf94876869ed1311d36
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-21 23:01:55 -0300

    Classe para ler e escrever em arquivos txt funcionando
    Took 16 minutes

--- src/main/java/com/atividades/LeituraEscritaArquivos.java
+++ src/main/java/com/atividades/LeituraEscritaArquivos.java
@@ -1,17 +1,46 @@
 package com.atividades;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 
 public class LeituraEscritaArquivos {
 
     BufferedReader buffer;
     BufferedWriter bufferWrite;
 
-
-
     public static void main(String[] args) {
-        LeituraEscritaArquivos leitura = new LeituraEscritaArquivos();
+
+        //Criação e Escrita do arquivo
+        File f = new File("ArquivoTeste.txt");
+        //Criação e Escrita do arquivo
+        try {
+            FileWriter fw = new FileWriter(f);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("Teste 1");
+            bw.newLine();
+            bw.write("Teste 2");
+            bw.newLine();
+            bw.write("Teste 3");
+
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Leitura do Arquivo
+        try {
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String s;
+
+            while((s = br.readLine()) != null){
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            System.out.println("###### Erro: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
