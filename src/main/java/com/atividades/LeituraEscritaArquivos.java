0be257ce879a7b10b34678b24efe55d52782218a
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-21 21:34:00 -0300

    Classe para ler e escrever em arquivos txt
    Took 9 minutes

---
+++ src/main/java/com/atividades/LeituraEscritaArquivos.java
@@ -0,0 +1,17 @@
+package com.atividades;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class LeituraEscritaArquivos {
+
+    BufferedReader buffer;
+    BufferedWriter bufferWrite;
+
+
+
+    public static void main(String[] args) {
+        LeituraEscritaArquivos leitura = new LeituraEscritaArquivos();
+    }
+
+}
